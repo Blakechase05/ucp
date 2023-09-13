178531d41b4516692100d7fbc9f576a6bcb1ae13
Blake <blakechase05@gmail.com>
2023-09-13 21:37:53 +0800

    Day 1 Progress - Printing Map to Screen W/O Border or anything using user input

--- main.c
+++ main.c
@@ -2,14 +2,38 @@
 #include <stdlib.h>
 
 int main(int argc, char* argv[]) {
-  int r = atoi(argv[1]);
-  int c = atoi(argv[2]);
-  
   int i, j = 0;
 
-  int (*arr)[c] = malloc(sizeof(int[r][c]));
+  int rows = atoi(argv[1]);
+  int cols = atoi(argv[2]);
+  
+  int **map;
 
-  free(arr);
- 
+  map = (int **)malloc(cols * sizeof(int *));
+  
+  for(i = 0; i < cols; i++) {
+    map[i] = (int*)malloc(rows * sizeof(int));
+  }
+
+  for (i = 0; i < cols; i++) {
+    for (j = 0; j < rows; j++) {
+      map[i][j] = 0;
+    }
+  }
+
+  for (i = 0; i < cols; i++) {
+    for (j = 0; j < rows; j++) {
+      printf(" %d", map[i][j]);
+    }
+    printf("\n");
+  }
+
+  for (i = 0; i < cols; i++) {
+    free(map[i]);
+  }
+  
+  free(map);
+
   return 0;
 }
+
