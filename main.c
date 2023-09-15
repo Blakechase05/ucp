91f9616f36e32f9d1402fe011330e01b2d12a7ff
Blake <blakechase05@gmail.com>
2023-09-15 23:02:39 +0800

    cls working instead of clear

--- main.c
+++ main.c
@@ -1,81 +1,9 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-#include "main.h"
-
-void init() {
-  system("clear");
-
-}
-
-int main(int argc, char* argv[]) {
-  int i, j = 0;
-
-  /* Input rows and coloums from the ./ command */
-  int rows = atoi(argv[1]) + 2;
-  int cols = atoi(argv[2]) + 2;
-  
-  /* Make a double pointer that will be our 2D Arrray*/
-  char **map;
-
-  map = (char**)malloc(cols * sizeof(char*));
-
-  if(cols % 2 == 0) {
-    printf("Even number of columns, cya l8r allig8r");
-    return 1; 
-  }
-
-  if(rows - 2 < 3) {
-    printf("The number of rows can't be less than 3, exiting program");
-    return 1;
-  }
-
-  if(cols - 2 < 5) {
-    printf("The number of columns can't be less than 5, exiting program");
-    return 1;
-  }
-
-  for(i = 0; i < cols; i++) {
-    map[i] = (char*)malloc(rows * sizeof(char));
-  }
-
-  init();
-
-  for (i = 0; i < cols; i++) {
-    for (j = 0; j < rows; j++) {
-      if(i % 2 == 0) {
-        map[i][j] = '.';
-      } else {
-        map[i][j] = ' ';
-      }
-    }
-  }
-
-  for (i = 0; i < rows; i++) {
-    map[0][i] = '*';
-    map[cols-1][i] = '*';
-  }
-
-  for (i = 0; i < cols; i++) {
-    map[i][0] = '*';
-    map[i][rows-1] = '*';
-  }
-
-  map[1][1] = 'P';
-  map[cols-2][rows-2] = 'G';
-
-  for (i = 0; i < cols; i++) {
-    for (j = 0; j < rows; j++) {
-      printf(" %c", map[i][j]);
-    }
-    printf("\n");
-  }
-
-  for (i = 0; i < cols; i++) {
-    free(map[i]);
-  }
-  
-  free(map);
-
-  return 0;
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    printf("Welcome To Geeksforgeeks\n");
+    system("cls");
+    printf("A computer science portal ");
+    return 0;
 }
