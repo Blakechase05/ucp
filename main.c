4682c9469fb7dc7f185338776f1715284816ba11
Blake <blakechase05@gmail.com>
2023-10-22 11:03:06 +0800

    oops

--- main.c
+++ main.c
@@ -1,78 +1,43 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-#include "init.h"
-#include "update.h"
-#include "random.h"
-#include "newSleep.h"
-#include "terminal.h"
-
-#define FALSE 0
-#define TRUE !FALSE
-
-int main(int argc, char* argv[]) {
-  int i = 0, j = 0;
-
-   /* Input rows and coloums from the ./ command */
-  int rows; 
-  int cols; 
-
-  if (argc != 3) {
-    printf("Usage: %s <cols> <rows>\n", argv[0]);
-    return 1;
-  }
-
-  rows= atoi(argv[2]) + 2;
-  cols = atoi(argv[1]) + 2;
-
-  /* Run all the init functions */
-  int* player = initPlayer();
-  int* cars = initCars(rows, cols);
-  char* carState = initCarState(cols);
-  char** map = initMap(rows, cols, player, cars, carState);
-
-  /* All of the checks with the input cols and rows */
-  if(cols % 2 == 0) {
-    printf("Even number of columns, exiting program");
-    return 0;
-  } else if(rows - 2 < 3) {
-    printf("The number of rows can't be less than 3, exiting program");
-    return 0;
-  } else if(cols - 2 < 5) {
-    printf("The number of columns can't be less than 5, exiting program");
-    return 0;
-  }
-
-  /* MAIN LOOP */
-  while(TRUE) {
-    updateMap(rows, cols, player, cars, carState, map);
-
-    /* Check for win condition */
-    if (player[1] == rows - 2) {
-      if (player[0] == cols - 2) {
-        printf("Winner Winner Chicken Dinner\n");
-        return 0;
-      }
+int main() {
+    float A = 0, B = 0;
+    float i, j;
+    int k;
+    float z[1760];
+    char b[1760];
+    printf("\x1b[2J");
+    for(;;) {
+        memset(b,32,1760);
+        memset(z,0,7040);
+        for(j=0; j < 6.28; j += 0.07) {
+            for(i=0; i < 6.28; i += 0.02) {
+                float c = sin(i);
+                float d = cos(j);
+                float e = sin(A);
+                float f = sin(j);
+                float g = cos(A);
+                float h = d + 2;
+                float D = 1 / (c * h * e + f * g + 5);
+                float l = cos(i);
+                float m = cos(B);
+                float n = sin(B);
+                float t = c * h * g - f * e;
+                int x = 40 + 30 * D * (l * h * m - t * n);
+                int y= 12 + 15 * D * (l * h * n + t * m);
+                int o = x + 80 * y;
+                int N = 8 * ((f * e - c * d * g) * m - c * d * e - f * g - l * d * n);
+                if(22 > y && y > 0 && x > 0 && 80 > x && D > z[o]) {
+                    z[o] = D;
+                    b[o] = ".,-~:;=!*#$@"[N > 0 ? N : 0];
+                }
+            }
+        }
+        printf("\x1b[H");
+        for(k = 0; k < 1761; k++) {
+            putchar(k % 80 ? b[k] : 10);
+            A += 0.00004;
+            B += 0.00002;
+        }
+        usleep(30000);
     }
-
-    /* Check for lose condition */
-    if(player[0] % 2 == 0) {
-      if(player[1] == cars[player[0]]) {
-        printf("You lost!\n");
-        return 0;
-      }
-    }
-  }
-
-  /* Master gave dobby a sock, dobby is freeeee   aka free the malloc'd memory :] */
-  for (i = 0; i < cols; i++) {
-    free(map[i]);
-  }
-
-  free(player);
-  free(cars);
-  free(carState);
-  free(map);
-
-  return 0;
+    return 0;
 }
